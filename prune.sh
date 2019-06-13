837d580d184d3110d2cdc7c61ebf18d0e87dcbba
Luca Terrazzan <luca.terraz@gmail.com>
2019-06-13 21:45:33 +0200

    first commit

---
+++ prune.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+echo "Start pruning..."
+
+agelimit=432000
+basepath="."
+
+# Parse options, if any
+while getopts "p:a:" flag; do
+  case "${flag}" in
+    p) basepath="${OPTARG}" ;;
+    a) agelimit="${OPTARG}" ;;
+  esac
+done
+
+# Check if the basepath is a valid directory
+if ! [ -d "$basepath" ]; then
+    echo "$basepath is not a valid directory. aborting."
+    exit
+fi
+
+for filename in $basepath/*; do
+
+  # Invalid file or empty folder, continue
+  [ -e "$filename" ] || continue
+
+  # Ignore directories
+  if [ -d "$filename" ]; then
+    echo "$filename is a directory, ignoring."
+    continue
+  fi
+
+  # Calculate file age
+  filelasteditdate=`date -r $filename +%s`
+  currenttime=`date +%s`
+  fileage=$((currenttime-filelasteditdate))
+
+  echo "$filename is $fileage seconds old"
+
+  # Remove if older than threshold
+  if [ $fileage -gt $agelimit ]; then
+    echo "$filename is older than $agelimit seconds, removing..."
+    rm $filename
+    echo "$filename removed"
+  # Otherwise do nothing
+  else
+    echo "$filename is younger than $agelimit seconds, keeping..."
+  fi
+
+done
+
+echo "Pruning completed!"
